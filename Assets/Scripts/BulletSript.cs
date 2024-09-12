c4a1c76b284ead4f723353b99860444f678cbdb2
Calin Catuta <calinut123@yahoo.co.uk>
2024-09-12 17:31:43 +0300

    Use the benefits of Interface.

---
+++ Assets/Scripts/BulletSript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSript : MonoBehaviour
+{
+    private float damage = 1f;
+
+    //This logic is implemented with the help of Interface.
+    //The Object will Collide with another Object and if that Object have the IDamageable
+    //then the Code will know witch Object it is and he will call the TakeDamage inside that Object.
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.TryGetComponent(out IDamageable target))
+        {
+            target.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
